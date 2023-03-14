dfdb7124cacd5c56c9cf89a6ab947dc4c5181325
Aditya_Kanojia <aditya05kanojia@gmail.com>
2023-03-14 23:58:35 +0530

    NavBar added

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/NavBar.css"
+function NavBar() {
+  return (
+    <>
+      <nav>
+        <h2>ShopKart</h2>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar;
